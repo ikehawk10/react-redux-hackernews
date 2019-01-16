88c2865a6768f6bff2ec7406751d146c42272bbe
Isaac <hawkins.isaac@gmail.com>
2019-01-16 17:02:02 -0600

    add a little styles

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -3,17 +3,54 @@ import './Stories.css'
 
 import Story from './Story';
 
+const COLUMNS = {
+  title: {
+    label: 'Title',
+    width: '40%',
+  },
+  author: {
+    label: 'Author',
+    width: '30%',
+  },
+  comments: {
+    label: 'Comments',
+    width: '10%',
+  },
+  points: {
+    label: 'Points',
+    width: '10%',
+  },
+  archive: {
+    width: '10%',
+  },
+};
+
 const Stories = ({ stories }) => {
   return (
-    <div className='stories'>
+    <div>
+      <StoriesHeader columns={COLUMNS} />
+
       {(stories || []).map(story => 
         <Story 
         key={story.objectID}
         story={story}
+        columns={COLUMNS}
         />
       )}
     </div>
   )
 }
+
+const StoriesHeader = ({ columns }) =>
+  <div className="stories-header">
+    {Object.keys(columns).map(key =>
+      <span
+        key={key}
+        style={{ width: columns[key].width }}
+      >
+        {columns[key].label}
+      </span>
+    )}
+  </div>
 
 export default Stories;
