4046b6b09b064c3f99c57f4aea63e74c571f4bf4
Isaac <hawkins.isaac@gmail.com>
2019-01-17 15:31:02 -0600

    combine reducers into root reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+
+import store from './store'
 import App from './components/App';
 import './index.css';
 
-const stories = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-ReactDOM.render(<App stories={stories} onArchive={() => {}} />, document.querySelector('#root'));
+ReactDOM.render(<App stories={store.getState().storyState} onArchive={() => {}} />, document.querySelector('#root'));
