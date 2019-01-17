f35b769f71079979e2df9a19c5ea581c1dbe575b
Isaac <hawkins.isaac@gmail.com>
2019-01-17 15:48:16 -0600

    create first action

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import store from './store'
 import App from './components/App';
 import './index.css';
 
-ReactDOM.render(<App stories={store.getState().storyState} onArchive={() => {}} />, document.querySelector('#root'));
+ReactDOM.render(
+  <App 
+    stories={store.getState().storyState} 
+    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id}) } />,
+  document.querySelector('#root'));
